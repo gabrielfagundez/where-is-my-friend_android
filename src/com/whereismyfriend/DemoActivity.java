9ffdeec8ffc2c15bf9674735704ae63604815741
Renzo Massobrio <renzomassobrio@gmail.com>
2013-11-18 00:30:06 -0200

    cambie api key de push, strings y logo de la push

--- src/com/whereismyfriend/DemoActivity.java
+++ src/com/whereismyfriend/DemoActivity.java
@@ -51,7 +51,7 @@ public class DemoActivity extends Activity {
      * Substitute you own sender ID here. This is the project number you got
      * from the API Console, as described in "Getting Started."
      */
-    String SENDER_ID = "602893169756";
+    String SENDER_ID = "1096662537091";
 
     /**
      * Tag used on log messages.
